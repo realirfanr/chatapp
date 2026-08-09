24b91b27cebcdfcce710760b074cdb79f5d13836
irfan rasool <realirfanr@gmail.com>
2026-08-09 09:51:29 +0500

    changes in db.js

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -6,7 +6,7 @@ export const connectDB = async()=>{
   try {
 
     mongoose.connection.on('connected', ()=> console.log('Database Connected'));
-    await mongoose.connect(`${process.env.MONGODB_URI}/chatappdb`)
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
     
   } catch (error) {
     console.log(error);
